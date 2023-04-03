52990ff9e461f139aeb580e711f7cc12133a049a
ChrisLubin <chrisx700@gmail.com>
2023-04-02 22:25:57 -0400

    Added FPS limit that's around 60 FPS

--- Assets/Scripts/Systems/Systems.cs
+++ Assets/Scripts/Systems/Systems.cs
@@ -1,3 +1,11 @@
+using UnityEngine;
+
 public class Systems : PersistentSingleton<Systems>
 {
+    protected override void Awake()
+    {
+        base.Awake();
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 45;
+    }
 }
