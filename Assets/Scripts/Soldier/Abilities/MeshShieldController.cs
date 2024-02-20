e48e05cc708726c9cdfa27f79f2b8a5e113bc1d6
ChrisLubin <chrisx700@gmail.com>
2024-02-20 00:22:54 -0500

    Added sound effect when a mesh shield is shot

--- Assets/Scripts/Soldier/Abilities/MeshShieldController.cs
+++ Assets/Scripts/Soldier/Abilities/MeshShieldController.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using static SoldierDamageController;
 
 public class MeshShieldController : MonoBehaviour, IDamageable
 {
-    void IDamageable.TakeLocalDamage(SoldierDamageController.DamageType _, int __, Vector3 ___, bool ____)
+    [SerializeField] private AudioClip _bulletImpactAudioClip;
+
+    private const float _BULLET_IMPACT_AUDIO_VOLUME = 0.1f;
+
+    void IDamageable.TakeLocalDamage(DamageType type, int _, Vector3 damagePoint, bool __)
     {
+        if (type == DamageType.Bullet)
+            AudioSource.PlayClipAtPoint(this._bulletImpactAudioClip, damagePoint, _BULLET_IMPACT_AUDIO_VOLUME);
     }
 }
