9f3953fba569a333fddf99a7ae0d8a3cb49e1d55
ChrisLubin <chrisx700@gmail.com>
2024-04-15 22:23:42 -0400

    Mouse sensitivity now changes based on ads

--- Assets/Scripts/Soldier/SoldierCameraController.cs
+++ Assets/Scripts/Soldier/SoldierCameraController.cs
@@ -1,12 +1,16 @@
 using Cinemachine;
+using InfimaGames.Animated.ModernGuns;
 using UnityEngine;
 
 public class SoldierCameraController : NetworkBehaviorAutoDisable<SoldierCameraController>
 {
+    private Character _character;
+
     [SerializeField] private Transform _neck;
     [SerializeField] private CinemachineVirtualCamera _firstPersonCamera;
     [SerializeField] private CinemachineVirtualCamera _thirdPersonCamera;
-    [SerializeField] private float _lookSpeed = 1f;
+    [SerializeField] private float _ADSlookSpeed = 0.5f;
+    [SerializeField] private float _nonADSlookSpeed = 1f;
     [SerializeField] private float _lookXLimit = 55f;
 
     private float _neckCenterRotationX;
@@ -17,6 +21,7 @@ public class SoldierCameraController : NetworkBehaviorAutoDisable<SoldierCameraC
 
     private void Awake()
     {
+        this._character = GetComponent<Character>();
         GameManager.OnStateChange += this.OnGameStateChange;
     }
 
@@ -45,10 +50,11 @@ public class SoldierCameraController : NetworkBehaviorAutoDisable<SoldierCameraC
     {
         if (PauseMenuController.IsPaused || GameManager.State == GameState.GameOver || SoldierKillStreakController.IS_USING_KILL_STREAK) { return; }
 
-        this._rotationX += -Input.GetAxis("Mouse Y") * this._lookSpeed;
+        float lookSpeed = Mathf.Lerp(this._nonADSlookSpeed, this._ADSlookSpeed, this._character.AimingAlpha);
+        this._rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
         this._rotationX = Mathf.Clamp(this._rotationX, this._neckCenterRotationX - this._lookXLimit, this._neckCenterRotationX + this._lookXLimit);
         this._neck.localRotation = Quaternion.Euler(this._rotationX, 0, 0);
-        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * this._lookSpeed, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
     }
 
     public void EnableFirstPersonCamera()
